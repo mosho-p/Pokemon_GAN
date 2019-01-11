ed4ea3d3df8f4cabfa65b8ac41451347b878f67d
Marshall Payne <34942223+mosho-p@users.noreply.github.com>
2019-01-11 17:46:14 -0600

    update

--- resize.py
+++ resize.py
@@ -2,13 +2,14 @@
 import os
 import cv2
 
-src = "./data" #pokeRGB_black
-dst = "./resizedData" # resized
+src = "./data"  # pokeRGB_black
+dst = "./resizedData"  # resized
 
 os.mkdir(dst)
 
 for each in os.listdir(src):
-    img = cv2.imread(os.path.join(src,each))
-    img = cv2.resize(img,(256,256))
-    cv2.imwrite(os.path.join(dst,each), img)
-    
+    img = cv2.imread(os.path.join(src, each), cv2.IMREAD_UNCHANGED)
+    for i in range(3):
+        img[:, :, i][img[:, :, 3] == 0] = 1
+    img = cv2.resize(img, (256, 256))
+    cv2.imwrite(os.path.join(dst, each), img[:, :, :3])
