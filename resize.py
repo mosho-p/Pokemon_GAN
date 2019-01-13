f93ed8f81f8ec22e629a84e61481f57422b04565
Marshall Payne <34942223+mosho-p@users.noreply.github.com>
2019-01-13 17:31:44 -0600

    run with only pokeGAN.py

--- resize.py
+++ resize.py
@@ -2,14 +2,15 @@
 import os
 import cv2
 
-src = "./data"  # pokeRGB_black
-dst = "./resizedData"  # resized
+def resize():
+    src = "./data"  # pokeRGB_black
+    dst = "./resizedData"  # resized
 
-os.mkdir(dst)
+    os.mkdir(dst)
 
-for each in os.listdir(src):
-    img = cv2.imread(os.path.join(src, each), cv2.IMREAD_UNCHANGED)
-    for i in range(3):
-        img[:, :, i][img[:, :, 3] == 0] = 1
-    img = cv2.resize(img, (256, 256))
-    cv2.imwrite(os.path.join(dst, each), img[:, :, :3])
+    for each in os.listdir(src):
+        img = cv2.imread(os.path.join(src, each), cv2.IMREAD_UNCHANGED)
+        for i in range(3):
+            img[:, :, i][img[:, :, 3] == 0] = 1
+        img = cv2.resize(img, (256, 256))
+        cv2.imwrite(os.path.join(dst, each), img[:, :, :3])
